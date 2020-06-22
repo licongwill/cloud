e7c34d76a64c70118efe8728083313a0f1e831a9
licong <licongcong.lcc@sunyur.com>
2020-06-22 20:22:11 +0800

    commit

---
+++ configure-server/src/main/java/cn/lic/cloud/configureserver/ConfigureServerApplication.java
@@ -0,0 +1,13 @@
+package cn.lic.cloud.configureserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigureServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigureServerApplication.class, args);
+	}
+
+}
---
+++ configure-server/src/test/java/cn/lic/cloud/configureserver/ConfigureServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lic.cloud.configureserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigureServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
