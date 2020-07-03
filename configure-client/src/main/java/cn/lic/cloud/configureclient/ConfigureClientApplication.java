91e2f4aabf7a99128e6ccc5ad6b1b14bf272f301
licong <licongcong.lcc@sunyur.com>
2020-07-03 15:23:56 +0800

    commit

---
+++ configure-client/src/main/java/cn/lic/cloud/configureclient/ConfigureClientApplication.java
@@ -0,0 +1,13 @@
+package cn.lic.cloud.configureclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigureClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigureClientApplication.class, args);
+	}
+
+}
---
+++ configure-client/src/test/java/cn/lic/cloud/configureclient/ConfigureClientApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lic.cloud.configureclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigureClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
