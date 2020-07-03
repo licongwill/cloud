087f9c53b16b6c8a7f699cb9dff56fdcfd4947c1
licong <licongcong.lcc@sunyur.com>
2020-07-03 15:35:19 +0800

    commit message

--- configure-server/src/main/java/cn/lic/cloud/configureserver/ConfigureServerApplication.java
+++ configure-server/src/main/java/cn/lic/cloud/configureserver/ConfigureServerApplication.java
@@ -2,9 +2,10 @@ package cn.lic.cloud.configureserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
-
 public class ConfigureServerApplication {
 
 	public static void main(String[] args) {
