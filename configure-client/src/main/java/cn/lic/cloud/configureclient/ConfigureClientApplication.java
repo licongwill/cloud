46e952c1686f0cce8b035784e8184ea1743d40f6
licong <licongcong.lcc@sunyur.com>
2020-07-07 09:41:28 +0800

    修改ConfigureClientApplication

--- configure-client/src/main/java/cn/lic/cloud/configureclient/ConfigureClientApplication.java
+++ configure-client/src/main/java/cn/lic/cloud/configureclient/ConfigureClientApplication.java
@@ -1,13 +1,17 @@
 package cn.lic.cloud.configureclient;
 
+import cn.lic.cloud.configureclient.listeners.MyApplicationContextListeners;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ConfigureClientApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigureClientApplication.class, args);
+		SpringApplication application = new SpringApplication(ConfigureClientApplication.class);
+		application.addListeners(new MyApplicationContextListeners());
+		ConfigurableApplicationContext context = application.run(args);
+
 	}
 
 }
