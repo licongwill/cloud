2d0505d364aa3d916939701fc8f1fcd5306706ef
licong <licongcong.lcc@sunyur.com>
2020-07-08 13:48:01 +0800

    修改ConfigureServerApplication

--- configure-server/src/main/java/cn/lic/cloud/configureserver/ConfigureServerApplication.java
+++ configure-server/src/main/java/cn/lic/cloud/configureserver/ConfigureServerApplication.java
@@ -1,15 +1,20 @@
 package cn.lic.cloud.configureserver;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigureServerApplication {
-
+	private static final Logger logger = LoggerFactory.getLogger(ConfigureServerApplication.class);
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigureServerApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(ConfigureServerApplication.class, args);
+		logger.info("context is {}",context);
 	}
 
 }
