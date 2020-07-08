feb9e512a66e8e9d9d54c202010a7d9ec4157408
licong <licongcong.lcc@sunyur.com>
2020-07-08 17:04:34 +0800

    修改ConfigureServerApplication

--- configure-client/src/main/java/cn/lic/cloud/configureclient/ConfigureClientApplication.java
+++ configure-client/src/main/java/cn/lic/cloud/configureclient/ConfigureClientApplication.java
@@ -4,9 +4,11 @@ import cn.lic.cloud.configureclient.listeners.MyApplicationContextListeners;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ConfigureClientApplication {
 	public static void main(String[] args) {
 		SpringApplication application = new SpringApplication(ConfigureClientApplication.class);
