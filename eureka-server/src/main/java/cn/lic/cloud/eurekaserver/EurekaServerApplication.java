a8feb721c1ce942963c66942b1344041de10a9e8
licong <licongcong.lcc@sunyur.com>
2020-07-25 12:02:14 +0800

    修改pom.xml

--- eureka-server/src/main/java/cn/lic/cloud/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/cn/lic/cloud/eurekaserver/EurekaServerApplication.java
@@ -1,13 +1,19 @@
-package cn.lic.cloud.netflix;
+package cn.lic.cloud.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * @author licong
+ * @version 1.0
+ * @date 2020/7/25 11:40
+ */
 @SpringBootApplication
-public class NetflixApplication {
+@EnableEurekaServer
+public class EurekaServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NetflixApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
 }
